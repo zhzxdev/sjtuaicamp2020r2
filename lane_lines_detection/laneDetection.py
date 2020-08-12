a9e18731a5bc136707184276ecbb2e2bcb36b2c0
skylee03 <1178715749@qq.com>
2020-08-12 14:57:53 +0800

    update laneDetection.py

--- lane_lines_detection/laneDetection.py
+++ lane_lines_detection/laneDetection.py
@@ -19,23 +19,21 @@ class camera:
 
     def spin(self):
         ret, img = self.cap.read()
-        if ret == True:
+        if ret:
             cv2.waitKey(1)
-
             # Convert Image
-            gray_Blur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray_blur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             kernel = np.ones((3, 3), np.uint8)
-            gray_Blur = cv2.erode(gray_Blur, kernel, iterations=1)
-            origin_thr = np.zeros_like(gray_Blur)
-            origin_thr[(gray_Blur >= 125)] = 255
+            gray_blur = cv2.erode(gray_blur, kernel, iterations=1)
+            origin_thr = np.zeros_like(gray_blur)
+            origin_thr[(gray_blur >= 125)] = 255
             src_points = np.array([[3, 570], [387, 460], [906, 452], [1041, 485]], dtype='float32')
             dst_points = np.array([[266., 686.], [266., 19.], [931., 20.], [931., 701.]], dtype='float32')
             M = cv2.getPerspectiveTransform(src_points, dst_points)
             binary_warped = cv2.warpPerspective(origin_thr, M, (1280, 720), cv2.INTER_LINEAR)
-
+            # 找左峰
             histogram_x = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0)  # Left Part
             lane_base = (list(filter(lambda x: histogram_x[x] > 30000, range(binary_warped.shape[0]))))[0]
-
             # 窗口生长算法
             window_height = int(binary_warped.shape[0] / nwindows)
             nonzero = binary_warped.nonzero()
@@ -55,26 +53,27 @@ class camera:
                     lane_current = int(np.mean(nonzerox[good_inds]))
                 elif window >= 3:
                     break
-
             lane_inds = np.concatenate(lane_inds)
-            pixelX = nonzerox[lane_inds]
-            pixelY = nonzeroy[lane_inds]
-
+            pixel_x = nonzerox[lane_inds]
+            pixel_y = nonzeroy[lane_inds]
             # calculate the aimPoint
-            if (pixelX.size == 0):
+            if pixel_x.size == 0:
                 return
+            front_distance = np.argsort(pixel_y)[int(len(pixel_y) / 8)]
+            aim_lane_p = [pixel_x[front_distance], pixel_y[front_distance]]
 
-            frontDistance = np.argsort(pixelY)[int(len(pixelY) / 8)]
-            aimLaneP = [pixelX[frontDistance], pixelY[frontDistance]]
-
-            self.aP[0] = aimLaneP[0] + delta
-            self.aP[1] = aimLaneP[1]
-            myK = (self.lastP[1] - self.aP[1]) / (self.lastP[0] - self.aP[0])
+            last_x = sorted(list(nonzerox[i] for i in range(len(nonzeroy)) if nonzeroy[i] == aim_lane_p[1]))[-1]
+            if last_x - aim_lane_p[0] > 40:
+                self.aP[0] = aim_lane_p[0] + delta
+            else:
+                self.aP[0] = aim_lane_p[0] - delta
+            self.aP[1] = aim_lane_p[1]
+            my_k = (self.lastP[1] - self.aP[1]) / (self.lastP[0] - self.aP[0])
             self.lastP = self.aP[:]
-            myTheta = math.atan(myK)
+            my_theta = math.atan(my_k)
             # 构造输出图像
-            cv2.putText(binary_warped, str(myTheta), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
-            cv2.circle(binary_warped, (int(aimLaneP[0]), int(aimLaneP[1])), 24, (0, 0, 0), 1)
+            cv2.putText(binary_warped, str(my_theta), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+            cv2.circle(binary_warped, (int(aim_lane_p[0]), int(aim_lane_p[1])), 24, (0, 0, 0), 1)
             cv2.circle(binary_warped, (int(self.aP[0]), int(self.aP[1])), 24, (255, 255, 255), 1)
             binary_warped = cv2.resize(binary_warped, (0, 0), fx=.5, fy=.5)
             cv2.imshow('pic', binary_warped)
