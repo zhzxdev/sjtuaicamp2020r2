b539eeceebc4cf6555ad63d58131018dd7ed919d
ZhangZisu <i@zzs1.cn>
2020-08-12 18:22:20 +0800

    Fix typo

--- ros_ws/src/lanecam/scripts/main.py
+++ ros_ws/src/lanecam/scripts/main.py
@@ -6,7 +6,7 @@ import rospy as r
 def realmain():
     r.init_node('lanecam', anonymous=True)
     rate = r.Rate(10)
-    while not rospy.is_shutdown():
+    while not r.is_shutdown():
         rate.sleep()
 
 
