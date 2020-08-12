bc933fb964840aef6713a812b1912404003f6f14
ZhangZisu <i@zzs1.cn>
2020-08-12 20:40:44 +0800

    Update

--- camera/camera.py
+++ camera/camera.py
@@ -1,19 +1,23 @@
-import cv2 as cv
-import time
+import cv2
 
-output_name = 'output.avi'
+cap = cv2.VideoCapture('/dev/video10')
+width = 1280
+ret = cap.set(3, width)
+height = 720
+ret = cap.set(4, height)
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+out = cv2.VideoWriter('out.mp4', fourcc, 20, (width, height))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret is True:
+        out.write(frame)
+        cv2.imshow('frame', frame)
+    else:
+        break
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
 
-if __name__ == '__main__':
-    camera = cv.VideoCapture(0)
-    fps = camera.get(cv.CAP_PROP_FPS)
-    width = int(camera.get(cv.CAP_PROP_FRAME_WIDTH))
-    height = int(camera.get(cv.CAP_PROP_FRAME_HEIGHT))
-    size = (width, height)
-    VWirte = cv.VideoWriter(output_name, cv.VideoWriter_fourcc('I', '4', '2', '0'), fps, size)
-    success, frame = camera.read()
-    while success:
-        VWirte.write(frame)
-        success, frame = camera.read()
-    time.sleep(1)
-    camera.release()
-    print('ok')
+cap.release()
+out.release()
+cv2.destroyAllWindows()
