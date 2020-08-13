7e52a48812cfe239f8dff6ae405b53236057b004
skylee03 <1178715749@qq.com>
2020-08-13 16:29:27 +0800

    update

--- lane_lines_detection/ckr.py
+++ lane_lines_detection/ckr.py
@@ -2,22 +2,11 @@ import cv2
 import numpy as np
 import time
 
-LL = 40
+block3 = 100
 RRR = 55
 LLL = 40
-half_width = 640
-half_height = 360
-
-delta = 150
-margin = 90
-minpix = 25
-nwindows = 12
-eps = 1e-4
-modifier = 0.3
-block = 20
-block2 = 305
-block3 = 100
-
+half_width = 320
+half_height = 180
 
 class camera:
     def __init__(self):
@@ -34,6 +23,7 @@ class camera:
 
     def spin(self):
         ret, img = self.cap.read()
+        img = cv2.resize(img, (0, 0), fx=.5, fy=.5)
         def show(direction):
             cv2.putText(img, direction, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
             cv2.imshow('img', img)
@@ -44,11 +34,11 @@ class camera:
             gray_blur = cv2.erode(gray_blur, kernel, iterations=1)
             origin_thr = np.zeros_like(gray_blur)
             origin_thr[(gray_blur >= 125)] = 255
-            binary_warped = origin_thr[360:720, ]
+            binary_warped = origin_thr[180:360, ]
             binary_warped = cv2.resize(binary_warped, (0, 0), fx=1, fy=2)
 
             histogram_x = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0)  # Left Part
-            lane_base = list(filter(lambda x: histogram_x[x] > 5000, range(len(histogram_x))))
+            lane_base = list(filter(lambda x: histogram_x[x] > 2500, range(len(histogram_x))))
 
             if len(lane_base) == 0:
                 self.d = 48
@@ -60,14 +50,14 @@ class camera:
             p = np.zeros(len(nonzero), dtype=int)
             cnt = 0
 
-            if 719 - nonzero[0][0] > block3:
+            if 359 - nonzero[0][0] > block3:
                 self.d = 48
                 show('center')
                 return self.d
-            hg = 719 - nonzero[0][0]
+            hg = 359 - nonzero[0][0]
 
             for i in range(len(nonzero)):
-                if 719 - nonzero[i][0] == hg:
+                if 359 - nonzero[i][0] == hg:
                     cnt += 1
                     p[cnt] = nonzero[i][1]
 
@@ -107,32 +97,3 @@ if __name__ == '__main__':
         cam.spin()
         time_t = time.time()
         print 'time: ' + str(time_t - time_s) + 's'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
