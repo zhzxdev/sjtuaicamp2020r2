da106084beb0feac17b18e807f0f2d111326146c
skylee03 <1178715749@qq.com>
2020-08-13 12:46:18 +0800

    update

--- ros_ws/src/lanecam/scripts/main.py
+++ ros_ws/src/lanecam/scripts/main.py
@@ -4,8 +4,13 @@ import rospy as r
 from std_msgs.msg import Int32
 
 import cv2
-import math
 import numpy as np
+
+LL = 40
+RRR = 55
+LLL = 40
+half_width = 640
+half_height = 360
 
 delta = 150
 margin = 90
@@ -13,6 +18,9 @@ minpix = 25
 nwindows = 12
 eps = 1e-4
 modifier = 0.3
+block = 20
+block2 = 305
+block3 = 100
 
 
 class camera:
@@ -20,101 +28,88 @@ class camera:
         self.d = 50.0
         self.x = 0.0
         self.last_my_theta = 0
-        # self.cap = cv2.VideoCapture('/dev/video10')
-        self.cap = cv2.VideoCapture('test2.mp4')
+        self.cap = cv2.VideoCapture('/dev/video10')
         self.aP = [0., 0.]
         self.lastP = [0., 0.]
-        self.width = 1280
-        self.ret = self.cap.set(3, self.width)
-        self.height = 720
-        self.ret = self.cap.set(4, self.height)
-        self.fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        self.out = cv2.VideoWriter('out.mp4', self.fourcc, 20, (self.width, self.height))
-        self.DEBUG = True
-        self.count = 0
 
     def __del__(self):
         self.cap.release()
 
     def spin(self):
-        self.count += 1
-        if self.count > 600:
-            self.out.release()
-            self.DEBUG = False
         ret, img = self.cap.read()
         if ret:
-            self.out.write(img)
-            cv2.imshow('raw', img)
+            cv2.waitKey(1)
             gray_blur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             kernel = np.ones((3, 3), np.uint8)
             gray_blur = cv2.erode(gray_blur, kernel, iterations=1)
             origin_thr = np.zeros_like(gray_blur)
             origin_thr[(gray_blur >= 125)] = 255
-            src_points = np.array([[3, 570], [387, 460], [906, 452], [1041, 485]], dtype='float32')
-            dst_points = np.array([[266., 686.], [266., 19.], [931., 20.], [931., 701.]], dtype='float32')
-            M = cv2.getPerspectiveTransform(src_points, dst_points)
-            binary_warped = cv2.warpPerspective(origin_thr, M, (1280, 720), cv2.INTER_LINEAR)
+            binary_warped = origin_thr[360:720, ]
+            binary_warped = cv2.resize(binary_warped, (0, 0), fx=1, fy=2)
 
             histogram_x = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0)  # Left Part
-            lane_base = list(filter(lambda x: histogram_x[x] > 2000, range(len(histogram_x))))
+            lane_base = list(filter(lambda x: histogram_x[x] > 5000, range(len(histogram_x))))
+
             if len(lane_base) == 0:
-                return 40
-            lane_base = lane_base[0]
+                self.d = 48
+                return self.d
 
-            window_height = int(binary_warped.shape[0] / nwindows)
             nonzero = binary_warped.nonzero()
-            nonzeroy = np.array(nonzero[0])
-            nonzerox = np.array(nonzero[1])
-            lane_current = lane_base
-            lane_inds = []
-            for window in range(nwindows):
-                win_y_low = binary_warped.shape[0] - (window + 1) * window_height
-                win_y_high = binary_warped.shape[0] - window * window_height
-                win_x_low = lane_current - margin
-                win_x_high = lane_current + margin
-                good_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_x_low) & (nonzerox < win_x_high)).nonzero()[0]
-                lane_inds.append(good_inds)
-                if len(good_inds) > minpix:
-                    lane_current = int(np.mean(nonzerox[good_inds]))
-                elif window >= 3:
-                    break
-            lane_inds = np.concatenate(lane_inds)
-            pixel_x = nonzerox[lane_inds]
-            pixel_y = nonzeroy[lane_inds]
-            # calculate the aimPoint
-            if pixel_x.size == 0:
-                return
-            front_distance = np.argsort(pixel_y)[int(len(pixel_y) / 8)]
-            aim_lane_p = [pixel_x[front_distance], pixel_y[front_distance]]
+            tmp = []
+            for i in range(len(nonzero[0])):
+                tmp.append([nonzero[0][i], nonzero[1][i]])
+            tmp.sort(lambda p, q: int(p[0] - q[0]))
+            nonzeroy = np.array(map(lambda p: p[0], tmp))
+            nonzerox = np.array(map(lambda p: p[1], tmp))
 
-            last_x = sorted(list(nonzerox[i] for i in range(len(nonzeroy)) if nonzeroy[i] == aim_lane_p[1]))[-1]
-            if last_x - aim_lane_p[0] > 100:
-                self.aP[0] = aim_lane_p[0] + delta
+            nonzeroy = map(lambda x: 719 - x, nonzeroy)
+            nonzerox = nonzerox[::-1]
+            nonzeroy = nonzeroy[::-1]
+
+            p = np.zeros_like(nonzerox)
+            cnt = 0
+
+            hg = -1
+            if nonzeroy[0] > block3:
+                hg = -1
             else:
-                self.aP[0] = aim_lane_p[0] - delta
-            self.aP[1] = aim_lane_p[1]
-            my_k = (self.lastP[1] - self.aP[1]) / (self.lastP[0] - self.aP[0])
-            self.lastP = self.aP[:]
-            my_theta = math.atan(my_k)
+                hg = nonzeroy[0]
 
-            if my_theta * self.last_my_theta < 0:
-                self.x = 0
-                self.d = 50
-            elif my_theta > 0:
-                self.x += modifier
-                self.d += self.x
-            elif my_theta < 0:
-                self.x -= modifier
-                self.d += self.x
-            self.last_my_theta = my_theta
+            if hg == -1:
+                self.d = 48
+                return self.d
 
-            if self.DEBUG:
-                cv2.putText(binary_warped, str(my_theta), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
-                cv2.circle(binary_warped, (int(aim_lane_p[0]), int(aim_lane_p[1])), 24, (0, 0, 0), 1)
-                cv2.circle(binary_warped, (int(self.aP[0]), int(self.aP[1])), 24, (255, 255, 255), 1)
-                binary_warped = cv2.resize(binary_warped, (0, 0), fx=.5, fy=.5)
-                cv2.imshow('out', binary_warped)
-            cv2.waitKey(100)
+            for i in range(len(nonzeroy)):
+                if nonzeroy[i] == hg:
+                    cnt += 1
+                    p[cnt] = nonzerox[i]
+
+            p[0] = 0
+            for i in range(len(nonzerox) - cnt - 1):
+                p[i + cnt + 1] = 10000000
+
+            p = sorted(p)
+            q = np.zeros_like(p)
+            num = 0
+            for i in range(1, cnt + 1):
+                if p[i] != p[i - 1] + 1 and i != 1:
+                    num += 1
+                    q[num] = i - 1
+            num += 1
+            q[num] = cnt
+
+            q[0] = -1
+            now = num
+
+            if now >= 2:
+                self.d = 48
+            else:
+                if p[q[1]] < half_width:
+                    self.d = RRR
+                else:
+                    self.d = LLL
+            return self.d
+
 
 
 def realmain():
